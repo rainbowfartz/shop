4188264d23ddb0e92679c9c410ccc0c8d7b5fca3
ToasterSSD <asherhol7@gmail.com>
2024-01-15 17:23:17 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,34 +11,32 @@ ALLOWED_EXTENSIONS = set(['jpeg', 'jpg', 'png'])
 @app.route('/')
 def home():
     return render_template("add.html")
+def getLoginDetails():
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        if 'email' not in session:
+            loggedIn = False
+            firstName = ''
+            noOfItems = 0
+        else:
+            loggedIn = True
+            cur.execute("SELECT userId, firstName FROM users WHERE email = ?", (session['email'], ))
+            userId, firstName = cur.fetchone()
+            cur.execute("SELECT count(productId) FROM kart WHERE userId = ?", (userId, ))
+            noOfItems = cur.fetchone()[0]
+    conn.close()
+    return (loggedIn, firstName, noOfItems)
 
-@app.route('/signup', methods= ["POST","GET"] )
-def index():
-    return render_template('signup.html')
+@app.route("/")
+def root():
+    loggedIn, firstName, noOfItems = getLoginDetails()
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute('SELECT productId, name, price, description, image, stock FROM products')
+        itemData = cur.fetchall()
+    itemData = parse(itemData)   
+    return render_template('home.html', itemData=itemData, loggedIn=loggedIn, firstName=firstName, noOfItems=noOfItems)
 
-@app.route('/login')
-def login2():
-    return render_template('login.html')
-
-@app.route('/login2', methods=['GET','POST'])
-def login2():
-    # email = request.form['email']
-    # password = request.form['password']
-    # print(f"{email}, {password}")
-    # db = shelve.open('user.db', 'r')
-    # users = db["Users"]
-    # if email in users:
-    #     print("found")
-    # else:
-    #     print("not found")
-   
-    # db.close()
-    
-    return render_template('login.html')
-
-def allowed_file(filename):
-    return '.' in filename and \
-            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route("/addItem", methods=["GET", "POST"])
 def addItem():
@@ -62,6 +60,7 @@ def addItem():
                 conn.rollback()
         conn.close()
         print(msg)
+        return redirect(url_for('root'))
 
 @app.route("/productDescription")
 def productDescription():
@@ -72,6 +71,190 @@ def productDescription():
         productData = cur.fetchone()
     conn.close()
     return render_template("products.html", data=productData)
+
+@app.route("/account/profile")
+def profileHome():
+    if 'email' not in session:
+        return redirect(url_for('root'))
+    loggedIn, firstName, noOfItems = getLoginDetails()
+    return render_template("profileHome.html", loggedIn=loggedIn, firstName=firstName, noOfItems=noOfItems)
+
+@app.route("/account/profile/edit")
+def editProfile():
+    if 'email' not in session:
+        return redirect(url_for('root'))
+    loggedIn, firstName, noOfItems = getLoginDetails()
+    with sqlite3.connect('database.db') as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT userId, email, firstName, lastName, address1, address2, zipcode, city, state, country, phone FROM users WHERE email = ?", (session['email'], ))
+        profileData = cur.fetchone()
+    conn.close()
+    return render_template("editProfile.html", profileData=profileData, loggedIn=loggedIn, firstName=firstName, noOfItems=noOfItems)   
+
+@app.route("/account/profile/changePassword", methods=["GET", "POST"])
+def changePassword():
+    if 'email' not in session:
+        return redirect(url_for('loginForm'))
+    if request.method == "POST":
+        oldPassword = request.form['oldpassword']
+        oldPassword = hashlib.md5(oldPassword.encode()).hexdigest()
+        newPassword = request.form['newpassword']
+        newPassword = hashlib.md5(newPassword.encode()).hexdigest()
+        with sqlite3.connect('database.db') as conn:
+            cur = conn.cursor()
+            cur.execute("SELECT userId, password FROM users WHERE email = ?", (session['email'], ))
+            userId, password = cur.fetchone()
+            if (password == oldPassword):
+                try:
+                    cur.execute("UPDATE users SET password = ? WHERE userId = ?", (newPassword, userId))
+                    conn.commit()
+                    msg="Changed successfully"
+                except:
+                    conn.rollback()
+                    msg = "Failed"
+                return render_template("changePassword.html", msg=msg)
+            else:
+                msg = "Wrong password"
+        conn.close()
+        return render_template("changePassword.html", msg=msg)
+    else:
+        return render_template("changePassword.html")  
+
+@app.route("/updateProfile", methods=["GET", "POST"])
+def updateProfile():
+    if request.method == 'POST':
+        email = request.form['email']
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        address1 = request.form['address1']
+        address2 = request.form['address2']
+        zipcode = request.form['zipcode']
+        city = request.form['city']
+        state = request.form['state']
+        country = request.form['country']
+        phone = request.form['phone']
+        with sqlite3.connect('database.db') as con:
+                try:
+                    cur = con.cursor()
+                    cur.execute('UPDATE users SET firstName = ?, lastName = ?, address1 = ?, address2 = ?, zipcode = ?, city = ?, state = ?, country = ?, phone = ? WHERE email = ?', (firstName, lastName, address1, address2, zipcode, city, state, country, phone, email))
+
+                    con.commit()
+                    msg = "Saved Successfully"
+                except:
+                    con.rollback()
+                    msg = "Error occured"
+        con.close()
+        return redirect(url_for('editProfile')) 
+    
+@app.route("/loginForm")
+def loginForm():
+    if 'email' in session:
+        return redirect(url_for('root'))
+    else:
+        return render_template('login.html', error='')
+
+@app.route("/login", methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if is_valid(email, password):
+            session['email'] = email
+            return redirect(url_for('root'))
+        else:
+            error = 'Invalid UserId / Password'
+            return render_template('login.html', error=error)
+        
+@app.route("/logout")
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('root'))
+
+def is_valid(email, password):
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    cur.execute('SELECT email, password FROM users')
+    data = cur.fetchall()
+    for row in data:
+        if row[0] == email and row[1] == hashlib.md5(password.encode()).hexdigest():
+            return True
+    return False
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        #Parse form data    
+        password = request.form['password']
+        email = request.form['email']
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        address1 = request.form['address1']
+        address2 = request.form['address2']
+        zipcode = request.form['zipcode']
+        city = request.form['city']
+        state = request.form['state']
+        country = request.form['country']
+        phone = request.form['phone']
+
+        with sqlite3.connect('database.db') as con:
+            try:
+                cur = con.cursor()
+                cur.execute('INSERT INTO users (password, email, firstName, lastName, address1, address2, zipcode, city, state, country, phone) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (hashlib.md5(password.encode()).hexdigest(), email, firstName, lastName, address1, address2, zipcode, city, state, country, phone))
+
+                con.commit()
+
+                msg = "Registered Successfully"
+            except:
+                con.rollback()
+                msg = "Error occured"
+        con.close()
+        return render_template("login.html", error=msg)
+
+@app.route("/registerationForm")
+def registrationForm():
+    return render_template("register.html")
+
+def parse(data):
+    ans = []
+    i = 0
+    while i < len(data):
+        curr = []
+        for j in range(7):
+            if i >= len(data):
+                break
+            curr.append(data[i])
+            i += 1
+        ans.append(curr)
+    return ans
+
+# @app.route('/signup', methods= ["POST","GET"] )
+# def index():
+#     return render_template('signup.html')
+
+# @app.route('/login')
+# def login2():
+#     return render_template('login.html')
+
+# @app.route('/login2', methods=['GET','POST'])
+# def login2():
+    # email = request.form['email']
+    # password = request.form['password']
+    # print(f"{email}, {password}")
+    # db = shelve.open('user.db', 'r')
+    # users = db["Users"]
+    # if email in users:
+    #     print("found")
+    # else:
+    #     print("not found")
+   
+    # db.close()
+    
+    # return render_template('login.html')
+
+def allowed_file(filename):
+    return '.' in filename and \
+            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 def parse(data):
     ans = []
