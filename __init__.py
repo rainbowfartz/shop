1b43b501c2401e2e855d2a840caadbf63fb18b03
FalconSans <153068920+FalconSans@users.noreply.github.com>
2024-01-22 14:59:02 +0800

    boing

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ def home():
 
 @app.route('/game')
 def game():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
