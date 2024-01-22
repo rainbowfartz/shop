dadbd0c3a90515907512698d4087fe0aa5e42401
ToasterSSD <asherhol7@gmail.com>
2024-01-22 15:01:10 +0800

    ll

--- Forms.py
+++ Forms.py
@@ -2,18 +2,9 @@ from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, v
 from wtforms.fields import EmailField, DateField
 
 class CreateUserForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
-    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
-    remarks = TextAreaField('Remarks', [validators.Optional()])
+    Full_name = StringField('Full_name', [validators.Length(min=1, max=150), validators.DataRequired()])
+
 
 class CreateCustomerForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    gender = SelectField('Gender', [validators.DataRequired()], choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
+    Full_name = StringField('Full_name', [validators.Length(min=1, max=150), validators.DataRequired()])
     email = EmailField('Email', [validators.Email(), validators.DataRequired()])
-    date_joined = DateField('Date Joined', format='%Y-%m-%d')
-    address = TextAreaField('Mailing Address', [validators.length(max=200), validators.DataRequired()])
-    membership = RadioField('Membership', choices=[('F', 'Fellow'), ('S', 'Senior'), ('P', 'Professional')], default='F')
-    remarks = TextAreaField('Remarks', [validators.Optional()])
--- User.py
+++ User.py
@@ -3,49 +3,36 @@ class User:
     count_id = 0
 
     # initializer method
-    def __init__(self, first_name, last_name, gender, membership, remarks):
+    def __init__(self, Fullname,Email,Password):
         User.count_id += 1
         self.__user_id = User.count_id
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__gender = gender
-        self.__membership = membership
-        self.__remarks = remarks
-
+        self.__Fullname = Fullname
+        self.__Email = Email
+        self.__Password = Password
     # accessor methods
     def get_user_id(self):
         return self.__user_id
 
     def get_first_name(self):
-        return self.__first_name
+        return self.__Fullname
 
-    def get_last_name(self):
-        return self.__last_name
-
-    def get_gender(self):
-        return self.__gender
-
-    def get_membership(self):
-        return self.__membership
-
-    def get_remarks(self):
-        return self.__remarks
+    def get_Email(self):
+        return self.__Email
+    
+    def get_Password(self):
+        return self.__Password
 
     # mutator methods
     def set_user_id(self, user_id):
         self.__user_id = user_id
 
-    def set_first_name(self, first_name):
-        self.__first_name = first_name
+    def set_first_name(self, Fullname):
+        self.__Fullname = Fullname
+        
+        
+    def set_first_name(self, Email):
+        self.__Email = Email
+    
+    def set_Password(self, Password):
+        self.__Password = Password
 
-    def set_last_name(self, last_name):
-        self.__last_name = last_name
-
-    def set_gender(self, gender):
-        self.__gender = gender
-
-    def set_membership(self, membership):
-        self.__membership = membership
-
-    def set_remarks(self, remarks):
-        self.__remarks = remarks
